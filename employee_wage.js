6b484809143f2e94af2275a996cdbe74d764e8dd
SHAILESH <shaileshdubeyr@gmail.com>
2021-11-24 21:30:26 +0530

    Added : code for employee wage using function

--- employee_wage.js
+++ employee_wage.js
@@ -5,18 +5,18 @@ const FULL_TIME_HOURS = 12;
 const WAGE_PER_HOUR = 20;
 
 let emphours = 0;
-empcheck = Math.floor((Math.random() * 10) % 2);
-switch (empcheck) {
-    case IS_PARTTIME:
-        emphours = PART_TIME_HOURS;
-        break;
-    case IS_FULLTIME:
-        emphours = FULL_TIME_HOURS;
-        break;
-    default:
-        emphours = 0;
-        break;
+empcheck = Math.floor((Math.random() * 10) % 3);
+let empWage = WAGE_PER_HOUR * employeeHours(empcheck);
+    console.log("the employee wage is " + empWage);
+    
+function employeeHours(){
+    switch (empcheck) {
+        case IS_PARTTIME:
+            return emphours = PART_TIME_HOURS;
+        case IS_FULLTIME:
+            return emphours = FULL_TIME_HOURS;
+        default:
+             return emphours = 0;
+    }
 }
 
-let empWage = WAGE_PER_HOUR * emphours;
-    console.log("the employee wage is " + empWage);
