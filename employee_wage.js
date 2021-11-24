9bb0ed5d7f821898cb53b1d5724e39e6ac10fa1f
SHAILESH <shaileshdubeyr@gmail.com>
2021-11-25 03:59:16 +0530

    Refactor : function name

--- employee_wage.js
+++ employee_wage.js
@@ -5,21 +5,23 @@ const FULL_TIME_HOURS = 12;
 const WAGE_PER_HOUR = 20;
 
 let emphours = 0;
+var totalwage;
 const NUMBER_OF_WOROKING_DAYS = 20;
-for(let i = 1; i <= NUMBER_OF_WOROKING_DAYS; i++){
+for (let i = 1; i <= NUMBER_OF_WOROKING_DAYS; i++) {
     empcheck = Math.floor((Math.random() * 10) % 3);
-    var empWage = WAGE_PER_HOUR * employeeHours(empcheck);
+    emphours += employeeWorkingHours(empcheck);
 }
+empWage = emphours * WAGE_PER_HOUR;
 console.log("the employee wage is " + empWage);
-    
-function employeeHours(){
+
+function employeeWorkingHours() {
     switch (empcheck) {
         case IS_PARTTIME:
             return emphours = PART_TIME_HOURS;
         case IS_FULLTIME:
             return emphours = FULL_TIME_HOURS;
         default:
-             return emphours = 0;
+            return emphours = 0;
     }
 }
 
